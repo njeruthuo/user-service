201d17e5d7b29bac64cd6749a269247382115d34
njeruthuo <juliusn411@gmail.com>
2026-08-22 10:20:25 +0300

    chore: added response headers to health endpoint

--- health.go
+++ health.go
@@ -10,6 +10,8 @@ type HealthResponse struct {
 }
 
 func GetHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(
 		&HealthResponse{Status: "system ok"},
 	)
